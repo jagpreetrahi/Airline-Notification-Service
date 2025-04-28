bc4f6b4ec2f53ec90a6acdf542e255a826dda54d
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-28 22:34:55 +0530

    FEAT:Added the custom helper function and object from other file

---
+++ src/utils/common/enum-data-types.js
@@ -0,0 +1,18 @@
+const Seat_types = {
+    BUSINESS : 'BUSINESS',
+    ECONOMY : 'ECONOMY',
+    FIRST_CLASS : 'FIRST CLASS',
+    PREMIUM_ECONOMY : 'PREMIUM ECONOMY'
+
+}
+
+const email_types = {
+    PENDING : 'pending',
+    CONFIRM : 'confirm',
+    FAILED : 'failed'
+}
+
+module.exports = {
+    Seat_types,
+    email_types
+}
